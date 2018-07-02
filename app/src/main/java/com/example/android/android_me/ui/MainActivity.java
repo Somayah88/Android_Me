f9ac3b0c3fa70334770ffa054376dc98d7115c9d
Somayah88 <somayah.alharbi@gmail.com>
2018-07-02 12:47:00 -0400

    Completed

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+/**
+ * Created by somayahalharbi on 7/2/18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
